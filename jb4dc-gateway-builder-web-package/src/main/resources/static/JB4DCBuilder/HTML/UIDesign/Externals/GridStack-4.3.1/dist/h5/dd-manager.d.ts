3af9c6a6f604bdd10c97a04ae64e1da9ee9e69b8
zhuibobo <zhuibobo@qq.com>
2022-02-20 20:50:29 +0800

    first commit

--- jb4dc-gateway-builder-web-package/src/main/resources/static/JB4DCBuilder/HTML/UIDesign/Externals/GridStack-4.3.1/dist/h5/dd-manager.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * dd-manager.ts 4.3.1-dev
- * Copyright (c) 2021 Alain Dumesny - see GridStack root license
- */
-import { DDDraggable } from './dd-draggable';
-export declare class DDManager {
-    static dragElement: DDDraggable;
-}
--- jb4dc-gateway-builder-web-package/src/main/resources/static/JB4DCBuilder/HTML/UIDesign/Externals/GridStack-4.3.1/dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * types.ts 4.3.1-dev
- * Copyright (c) 2021 Alain Dumesny - see GridStack root license
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
